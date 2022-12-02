2eaa6c3acc2f79474ea721160032f0594b558775
Patrick Nelsen <pjn@rincon.com>
2022-12-02 15:03:58 -0700

    feat: solve day 1 part 2

--- 01/01.go
+++ 01/01.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"sort"
 )
 
 func check(e error) {
@@ -36,6 +37,40 @@ func part1(scanner *bufio.Scanner) int {
 	return most_calories
 }
 
+func sum(array []int) int {
+	result := 0
+
+	for _, v:= range array {
+		result += v
+	}
+
+	return result
+}
+
+func part2(scanner *bufio.Scanner) int {
+	top_calories := []int{0, 0, 0}
+	curr_calories := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if line != "" {
+			line_calories, err := strconv.Atoi(line)
+			check(err)
+			curr_calories += line_calories
+		} else {
+			curr_calories = 0
+		}
+
+		if curr_calories > top_calories[0] {
+			top_calories[0] = curr_calories
+			sort.Ints(top_calories)
+		}
+	}
+
+	return sum(top_calories)
+}
+
 func main() {
 	args := os.Args[1:]
 	file_path := args[0]
@@ -46,6 +81,15 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	result := part1(scanner)
+
+	fmt.Println(result)
+
+	file, err = os.Open(file_path)
+	check(err)
+
+	scanner = bufio.NewScanner(file)
+
+	result = part2(scanner)
 
 	fmt.Println(result)
 }
