9af184d6a672b466b6638beb1fe3d62fed65c2b3
Patrick Nelsen <pnelsen129@gmail.com>
2022-12-04 21:06:50 -0700

    feat: solve day 4 part 1

---
+++ 04/04.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func get_sections_from_assignment_pair(assignment_pair string) [4]int {
+	var sections [4]int
+	assignments := strings.Split(assignment_pair, ",")
+
+	for i := 0; i < 2; i++ {
+		assignment_sections := assignments[i]
+		assignment_sections_split := strings.Split(assignment_sections, "-")
+		section_1, err := strconv.Atoi(assignment_sections_split[0])
+		check(err)
+		section_2, err := strconv.Atoi(assignment_sections_split[1])
+		check(err)
+
+		sections[i*2] = section_1
+		sections[i*2+1] = section_2
+	}
+
+	return sections
+}
+
+func part1(file_name string) int {
+	total_score := 0
+
+	file, err := os.Open(file_name)
+	check(err)
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		sections := get_sections_from_assignment_pair(line)
+
+		if sections[0] <= sections[2] && sections[1] >= sections[3] {
+			total_score += 1
+		} else if sections[2] <= sections[0] && sections[3] >= sections[1] {
+			total_score += 1
+		}
+	}
+
+	return total_score
+}
+
+func part2(file_name string) int {
+	total_score := 0
+
+	// file, err := os.Open(file_name)
+	// check(err)
+
+	// scanner := bufio.NewScanner(file)
+
+	// for scanner.Scan() {
+	// 	line := scanner.Text()
+	// }
+
+	return total_score
+}
+
+func main() {
+	args := os.Args[1:]
+	file_path := args[0]
+
+	fmt.Println(part1(file_path))
+
+	fmt.Println(part2(file_path))
+}
