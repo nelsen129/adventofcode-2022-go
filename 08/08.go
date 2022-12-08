7419d9872853a90d4ebf9b2bd3dec6d1c24cfb49
Patrick Nelsen <pjn@rincon.com>
2022-12-08 10:39:56 -0700

    feat: solve day 8 part 2

--- 08/08.go
+++ 08/08.go
@@ -123,19 +123,148 @@ func part1(file_name string) int {
 	return total_visible
 }
 
+func get_scenic_score_up(tree_map [][]int, scenic_map [][]uint8, x int, y int) uint8 {
+	if scenic_map[x][y] != 0 {
+		return scenic_map[x][y]
+	}
+	if x == 0 {
+		return scenic_map[x][y]
+	}
+
+	i := x - 1
+	scenic_score := uint8(1)
+	for i > 0 {
+		if tree_map[x][y] <= tree_map[i][y] {
+			break
+		}
+
+		scenic_score += get_scenic_score_up(tree_map, scenic_map, i, y)
+		i = x - int(scenic_score)
+	}
+
+	scenic_map[x][y] = scenic_score
+
+	return scenic_map[x][y]
+}
+
+func get_scenic_score_left(tree_map [][]int, scenic_map [][]uint8, x int, y int) uint8 {
+	if scenic_map[x][y] != 0 {
+		return scenic_map[x][y]
+	}
+	if y == 0 {
+		return scenic_map[x][y]
+	}
+
+	i := y - 1
+	scenic_score := uint8(1)
+	for i > 0 {
+		if tree_map[x][y] <= tree_map[x][i] {
+			break
+		}
+
+		scenic_score += get_scenic_score_left(tree_map, scenic_map, x, i)
+		i = y - int(scenic_score)
+	}
+
+	scenic_map[x][y] = scenic_score
+
+	return scenic_map[x][y]
+}
+
+func get_scenic_score_right(tree_map [][]int, scenic_map [][]uint8, x int, y int) uint8 {
+	if scenic_map[x][y] != 0 {
+		return scenic_map[x][y]
+	}
+	if y == len(tree_map[0])-1 {
+		return scenic_map[x][y]
+	}
+
+	i := y + 1
+	scenic_score := uint8(1)
+	for i < len(tree_map[0])-1 {
+		if tree_map[x][y] <= tree_map[x][i] {
+			break
+		}
+
+		scenic_score += get_scenic_score_right(tree_map, scenic_map, x, i)
+		i = y + int(scenic_score)
+	}
+
+	scenic_map[x][y] = scenic_score
+
+	return scenic_map[x][y]
+}
+
+func get_scenic_score_down(tree_map [][]int, scenic_map [][]uint8, x int, y int) uint8 {
+	if scenic_map[x][y] != 0 {
+		return scenic_map[x][y]
+	}
+	if x == len(tree_map)-1 {
+		return scenic_map[x][y]
+	}
+
+	i := x + 1
+	scenic_score := uint8(1)
+	for i < len(tree_map)-1 {
+		if tree_map[x][y] <= tree_map[i][y] {
+			break
+		}
+
+		scenic_score += get_scenic_score_down(tree_map, scenic_map, i, y)
+		i = x + int(scenic_score)
+	}
+
+	scenic_map[x][y] = scenic_score
+
+	return scenic_map[x][y]
+}
+
 func part2(file_name string) int {
-	total_score := 0
+	max_scenic_score := 0
+	var tree_map [][]int
 
-	// file, err := os.Open(file_name)
-	// check(err)
+	file, err := os.Open(file_name)
+	check(err)
 
-	// scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(file)
 
-	// for scanner.Scan() {
-	// 	line := scanner.Text()
-	// }
+	for scanner.Scan() {
+		line := scanner.Text()
 
-	return total_score
+		line_split := strings.Split(line, "")
+		line_int := make([]int, len(line_split))
+
+		for i := range line_split {
+			line_int[i], err = strconv.Atoi(line_split[i])
+			check(err)
+		}
+
+		tree_map = append(tree_map, line_int)
+	}
+
+	// Create scenic score maps
+	scenic_maps := make([][][]uint8, 4) // ulrd
+	for i := 0; i < 4; i++ {
+		scenic_maps[i] = make([][]uint8, len(tree_map))
+		for j := 0; j < len(tree_map); j++ {
+			scenic_maps[i][j] = make([]uint8, len(tree_map[0]))
+		}
+	}
+
+	for x := 0; x < len(tree_map); x++ {
+		for y := 0; y < len(tree_map[x]); y++ {
+			scenic_score := 1
+			scenic_score *= int(get_scenic_score_up(tree_map, scenic_maps[0], x, y))
+			scenic_score *= int(get_scenic_score_left(tree_map, scenic_maps[1], x, y))
+			scenic_score *= int(get_scenic_score_right(tree_map, scenic_maps[2], x, y))
+			scenic_score *= int(get_scenic_score_down(tree_map, scenic_maps[3], x, y))
+			if scenic_score > max_scenic_score {
+				max_scenic_score = scenic_score
+			}
+		}
+	}
+
+	return max_scenic_score
 }
 
 func main() {
