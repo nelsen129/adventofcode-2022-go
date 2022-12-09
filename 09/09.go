7bc634657fd70a591885232f67f8f9e1a0107738
Patrick Nelsen <pnelsen129@gmail.com>
2022-12-08 22:34:02 -0700

    feat: solve day 9 part 1

--- 09/09.go
+++ 09/09.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -12,19 +15,68 @@ func check(e error) {
 	}
 }
 
+func move_head(Hx int, Hy int, dir string) [2]int {
+	if dir == "U" {
+		return [2]int{Hx, Hy + 1}
+	} else if dir == "L" {
+		return [2]int{Hx - 1, Hy}
+	} else if dir == "R" {
+		return [2]int{Hx + 1, Hy}
+	} else if dir == "D" {
+		return [2]int{Hx, Hy - 1}
+	} else {
+		return [2]int{Hx, Hy}
+	}
+}
+
+func move_tail(Hx, Hy, Tx, Ty int) [2]int {
+	if Hx-Tx < 2 && Tx-Hx < 2 && Hy-Ty < 2 && Ty-Hy < 2 {
+		return [2]int{Tx, Ty}
+	}
+
+	if Hx > Tx {
+		Tx += 1
+	} else if Hx < Tx {
+		Tx -= 1
+	}
+
+	if Hy > Ty {
+		Ty += 1
+	} else if Hy < Ty {
+		Ty -= 1
+	}
+
+	return [2]int{Tx, Ty}
+}
+
 func part1(file_name string) int {
-	total_score := 0
+	H := [2]int{0, 0}
+	T := [2]int{0, 0}
 
-	// file, err := os.Open(file_name)
-	// check(err)
+	visited := make(map[[2]int]byte)
 
-	// scanner := bufio.NewScanner(file)
+	visited[T] = 1
 
-	// for scanner.Scan() {
-	// 	line := scanner.Text()
-	// }
+	file, err := os.Open(file_name)
+	check(err)
 
-	return total_score
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		line_split := strings.Split(line, " ")
+		dir := line_split[0]
+		dist, err := strconv.Atoi(line_split[1])
+		check(err)
+
+		for i := 0; i < dist; i++ {
+			H = move_head(H[0], H[1], dir)
+			T = move_tail(H[0], H[1], T[0], T[1])
+			visited[T] = 1
+		}
+	}
+
+	return len(visited)
 }
 
 func part2(file_name string) int {
