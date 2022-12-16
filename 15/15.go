a3a25316cdb9d04da57a838d0b2c2b305e017c6c
Patrick Nelsen <pjn@rincon.com>
2022-12-15 17:47:30 -0700

    feat: started to solve day 15. this stil needs some work

---
+++ 15/15.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/nelsen129/adventofcode-2022-go/15/beaconmap"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func parseSensorString(sensor_string string) complex128 {
+	sensor_string_strip := strings.TrimLeft(sensor_string, "Sensor at x=")
+	sensor_string_split := strings.Split(sensor_string_strip, ", y=")
+
+	x, err := strconv.Atoi(sensor_string_split[0])
+	check(err)
+	y, err := strconv.Atoi(sensor_string_split[1])
+	check(err)
+
+	return complex(float64(x), float64(y))
+}
+
+func parseBeaconString(beacon_string string) complex128 {
+	beacon_string_strip := strings.TrimLeft(beacon_string, "closest beacon is at x=")
+	beacon_string_split := strings.Split(beacon_string_strip, ", y=")
+
+	x, err := strconv.Atoi(beacon_string_split[0])
+	check(err)
+	y, err := strconv.Atoi(beacon_string_split[1])
+	check(err)
+
+	return complex(float64(x), float64(y))
+}
+
+func getSensorBeaconList(file_name string) [][2]complex128 {
+	var sensor_beacon_list [][2]complex128
+
+	file, err := os.Open(file_name)
+	check(err)
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			continue
+		}
+
+		line_split := strings.Split(line, ": ")
+		sensor := parseSensorString(line_split[0])
+		beacon := parseBeaconString(line_split[1])
+
+		sensor_beacon := [2]complex128{sensor, beacon}
+		sensor_beacon_list = append(sensor_beacon_list, sensor_beacon)
+	}
+	return sensor_beacon_list
+}
+
+func part1(file_name string) int {
+	sensor_beacon_list := getSensorBeaconList(file_name)
+	beaconmap := beaconmap.NewBeaconMap()
+
+	for i := range sensor_beacon_list {
+		beaconmap.AddSensor(sensor_beacon_list[i][0], sensor_beacon_list[i][1])
+	}
+
+	fmt.Println(beaconmap)
+	fmt.Println(len(beaconmap.GetBeacons()))
+
+	return beaconmap.GetBeaconCoverageAtRow(10)
+}
+
+func part2(file_name string) int {
+	total_score := 0
+
+	// file, err := os.Open(file_name)
+	// check(err)
+
+	// scanner := bufio.NewScanner(file)
+
+	// for scanner.Scan() {
+	// 	line := scanner.Text()
+	// }
+
+	return total_score
+}
+
+func main() {
+	start := time.Now()
+
+	args := os.Args[1:]
+	file_path := args[0]
+
+	fmt.Println("Part 1:", part1(file_path))
+
+	fmt.Println("Part 2:", part2(file_path))
+
+	duration := time.Since(start)
+
+	fmt.Println("Program execution time:", duration)
+}
---
+++ 15/beaconmap/beaconmap.go
@@ -0,0 +1,83 @@
+package beaconmap
+
+import (
+	"fmt"
+	"math"
+)
+
+type BeaconMap struct {
+	beacons map[complex128]rune
+	max_x   float64
+	min_x   float64
+}
+
+func NewBeaconMap() *BeaconMap {
+	beaconmap := BeaconMap{}
+	beaconmap.beacons = make(map[complex128]rune)
+
+	return &beaconmap
+}
+
+func (B *BeaconMap) checkPoint(point complex128) bool {
+	_, ok := B.beacons[point]
+	return ok
+}
+
+func (B *BeaconMap) addPoints(sensor complex128, dist float64) {
+	B.beacons[sensor+complex(dist, 0)] = '#'
+	B.beacons[sensor-complex(dist, 0)] = '#'
+	B.beacons[sensor+complex(0, dist)] = '#'
+	B.beacons[sensor-complex(0, dist)] = '#'
+
+	fmt.Println(sensor, complex(dist, 0), complex(0, dist))
+	fmt.Println(dist)
+
+	if real(sensor)+dist > B.max_x {
+		B.max_x = real(sensor) + dist
+	}
+	if real(sensor)-dist < B.min_x {
+		B.min_x = real(sensor) - dist
+	}
+
+	fmt.Println(B.beacons)
+}
+
+func (B *BeaconMap) AddSensor(sensor, beacon complex128) {
+	B.beacons[sensor] = 'S'
+
+	dist := getManhattanDist(sensor, beacon)
+
+	fmt.Println(sensor, beacon)
+	fmt.Println(B.beacons)
+
+	for i := float64(1); i < dist; i += 1 {
+		fmt.Println("adding dist", sensor, i)
+		B.addPoints(sensor, i)
+	}
+
+	B.beacons[beacon] = 'B'
+}
+
+func (B *BeaconMap) GetBeacons() map[complex128]rune {
+	return B.beacons
+}
+
+func (B *BeaconMap) GetBeaconCoverageAtRow(row float64) int {
+	total_coverage := 0
+	min_coord := complex(B.min_x, row)
+	max_coord := complex(B.max_x, row)
+	fmt.Println(min_coord, max_coord)
+
+	for i := min_coord; real(i) <= real(max_coord); i += 1 + 0i {
+		fmt.Println("checking", i)
+		if B.checkPoint(i) {
+			total_coverage++
+		}
+	}
+
+	return total_coverage
+}
+
+func getManhattanDist(p1, p2 complex128) float64 {
+	return math.Abs(real(p1)-real(p2)) + math.Abs(imag(p1)-imag(p2))
+}
