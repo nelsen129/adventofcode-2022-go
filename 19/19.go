d194f22cbaa4a5f1096d435ab40e0dd5e1f80b42
Patrick Nelsen <pjn@rincon.com>
2022-12-19 11:21:19 -0700

    feat: solve day 19 part 1, although it is quite slow

---
+++ 19/19.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/nelsen129/adventofcode-2022-go/19/blueprint"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func parseIdFromString(id_string string) int {
+	id_string = strings.TrimPrefix(id_string, "Blueprint ")
+	id_string = strings.TrimSuffix(id_string, ":")
+	id, err := strconv.Atoi(id_string)
+	check(err)
+	return id
+}
+
+func parseRobotFromString(robot_string string) []int {
+	robot_costs := make([]int, 3)
+	robot_string = strings.Split(robot_string, " costs ")[1]
+	robot_string = strings.TrimSuffix(robot_string, ".")
+	resource_strings := strings.Split(robot_string, " and ")
+
+	for i := range resource_strings {
+		cost_string := strings.Split(resource_strings[i], " ")[0]
+		cost, err := strconv.Atoi(cost_string)
+		check(err)
+		if strings.Contains(resource_strings[i], "ore") {
+			robot_costs[0] = cost
+		} else if strings.Contains(resource_strings[i], "clay") {
+			robot_costs[1] = cost
+		} else if strings.Contains(resource_strings[i], "obsidian") {
+			robot_costs[2] = cost
+		}
+	}
+
+	return robot_costs
+}
+
+func getBlueprintsFromFileName(file_name string) []*blueprint.Blueprint {
+	var blueprints []*blueprint.Blueprint
+	file, err := os.Open(file_name)
+	check(err)
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		line_split := strings.Split(line, " Each ")
+		id := parseIdFromString(line_split[0])
+		var robots [][]int
+		for i := 1; i < len(line_split); i++ {
+			robots = append(robots, parseRobotFromString(line_split[i]))
+		}
+		blueprint := blueprint.NewBlueprint(id, robots)
+		blueprints = append(blueprints, blueprint)
+	}
+
+	return blueprints
+}
+
+func part1(file_name string) int {
+	total_quality := 0
+
+	blueprints := getBlueprintsFromFileName(file_name)
+	fmt.Println(blueprints)
+	for i := range blueprints {
+		fmt.Println(blueprints[i])
+		total_quality += blueprints[i].GetGeodeProduction(24)
+	}
+
+	return total_quality
+}
+
+func part2(file_name string) int {
+	total_score := 0
+
+	// file, err := os.Open(file_name)
+	// check(err)
+
+	// scanner := bufio.NewScanner(file)
+
+	// for scanner.Scan() {
+	// 	line := scanner.Text()
+	// }
+
+	return total_score
+}
+
+func main() {
+	start := time.Now()
+
+	args := os.Args[1:]
+	file_path := args[0]
+
+	fmt.Println("Part 1:", part1(file_path))
+
+	fmt.Println("Part 2:", part2(file_path))
+
+	duration := time.Since(start)
+
+	fmt.Println("Program execution time:", duration)
+}
---
+++ 19/blueprint/blueprint.go
@@ -0,0 +1,166 @@
+package blueprint
+
+import "fmt"
+
+type Blueprint struct {
+	id          int
+	robot_costs [][]int
+}
+
+type BlueprintStackItem struct {
+	blueprint      *Blueprint
+	curr_robots    []int
+	curr_resources []int
+	curr_time      int
+}
+
+func NewBlueprint(id int, robot_costs [][]int) *Blueprint {
+	var B Blueprint
+	B.id = id
+	for i := range robot_costs {
+		B.robot_costs = append(B.robot_costs, robot_costs[i])
+	}
+
+	return &B
+}
+
+func (Bsi *BlueprintStackItem) copyBlueprintStackItem() *BlueprintStackItem {
+	new_bsi := BlueprintStackItem{
+		blueprint:      Bsi.blueprint,
+		curr_robots:    make([]int, len(Bsi.curr_robots)),
+		curr_resources: make([]int, len(Bsi.curr_resources)),
+		curr_time:      Bsi.curr_time,
+	}
+	copy(new_bsi.curr_robots, Bsi.curr_robots)
+	copy(new_bsi.curr_resources, Bsi.curr_resources)
+
+	return &new_bsi
+}
+
+func (Bsi *BlueprintStackItem) getTimeUntilRobot(robot_index int) (int, bool) {
+	var robot_time int
+	for i := range Bsi.blueprint.robot_costs[robot_index] {
+		cost_diff := Bsi.blueprint.robot_costs[robot_index][i] - Bsi.curr_resources[i]
+		if Bsi.curr_robots[i] == 0 && cost_diff <= 0 {
+			continue
+		} else if Bsi.curr_robots[i] == 0 {
+			return 0, false
+		}
+		curr_robot_time := (cost_diff + Bsi.curr_robots[i] - 1) / Bsi.curr_robots[i]
+		if curr_robot_time > robot_time {
+			robot_time = curr_robot_time
+		}
+	}
+
+	return robot_time + 1, true
+}
+
+func (Bsi *BlueprintStackItem) advanceTime(time int) {
+	Bsi.curr_time += time
+	for i := range Bsi.curr_robots {
+		Bsi.curr_resources[i] += Bsi.curr_robots[i] * time
+	}
+}
+
+func (Bsi *BlueprintStackItem) purchaseRobot(robot_index int) {
+	Bsi.curr_robots[robot_index]++
+	for i := range Bsi.blueprint.robot_costs[robot_index] {
+		Bsi.curr_resources[i] -= Bsi.blueprint.robot_costs[robot_index][i]
+	}
+}
+
+func (Bsi *BlueprintStackItem) getNextBlueprintStacks(max_time int) []*BlueprintStackItem {
+	var next_blueprint_stack []*BlueprintStackItem
+
+	// option for no decision, just passing time
+	if Bsi.curr_time < max_time {
+		next_blueprint_stack_item := Bsi.copyBlueprintStackItem()
+		next_blueprint_stack_item.advanceTime(max_time - Bsi.curr_time)
+		next_blueprint_stack = append(next_blueprint_stack, next_blueprint_stack_item)
+	}
+
+	// check which robot to build next
+	for i := range Bsi.curr_robots {
+		robot_time, ok := Bsi.getTimeUntilRobot(i)
+		if !ok {
+			continue
+		}
+		if robot_time <= 0 {
+			robot_time = 1
+		}
+		if Bsi.curr_time+robot_time > max_time {
+			continue
+		}
+		next_blueprint_stack_item := Bsi.copyBlueprintStackItem()
+		next_blueprint_stack_item.advanceTime(robot_time)
+		next_blueprint_stack_item.purchaseRobot(i)
+
+		next_blueprint_stack = append(next_blueprint_stack, next_blueprint_stack_item)
+	}
+
+	return next_blueprint_stack
+}
+
+func (Bsi *BlueprintStackItem) isWorseThan(other_bsi *BlueprintStackItem) bool {
+	if Bsi.curr_time < other_bsi.curr_time {
+		return false
+	}
+	for i := range Bsi.curr_robots {
+		if Bsi.curr_robots[i] > other_bsi.curr_robots[i] {
+			return false
+		}
+	}
+	for i := range Bsi.curr_resources {
+		if Bsi.curr_resources[i] > other_bsi.curr_resources[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func (B *Blueprint) GetGeodeProduction(time int) int {
+	var max_geodes int
+	first_stack_item := BlueprintStackItem{
+		blueprint:      B,
+		curr_robots:    make([]int, len(B.robot_costs)),
+		curr_resources: make([]int, len(B.robot_costs)),
+	}
+	first_stack_item.curr_robots[0] = 1
+	blueprint_stack := []*BlueprintStackItem{&first_stack_item}
+
+	best_stack_item_map := make(map[int]*BlueprintStackItem)
+
+	for len(blueprint_stack) != 0 {
+		curr_stack_item := blueprint_stack[len(blueprint_stack)-1]
+		blueprint_stack = blueprint_stack[:len(blueprint_stack)-1]
+
+		if curr_stack_item.curr_time > time {
+			continue
+		}
+		if curr_stack_item.curr_resources[len(curr_stack_item.curr_resources)-1] > max_geodes {
+			max_geodes = curr_stack_item.curr_resources[len(curr_stack_item.curr_resources)-1]
+			fmt.Println("new max geodes at", curr_stack_item)
+		}
+		if curr_stack_item.curr_time == time {
+			continue
+		}
+
+		curr_best, ok := best_stack_item_map[curr_stack_item.curr_time]
+		if !ok || curr_best.isWorseThan(curr_stack_item) {
+			for i := curr_stack_item.curr_time; i <= time; i++ {
+				curr_best, ok := best_stack_item_map[curr_stack_item.curr_time]
+				if !ok || curr_best.isWorseThan(curr_stack_item) {
+					best_stack_item_map[i] = curr_stack_item
+				} else {
+					break
+				}
+			}
+		} else if curr_stack_item.isWorseThan(curr_best) {
+			continue
+		}
+
+		blueprint_stack = append(blueprint_stack, curr_stack_item.getNextBlueprintStacks(time)...)
+	}
+
+	return max_geodes * B.id
+}
